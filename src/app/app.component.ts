4891865b4cb354bd62b1fae161970c080dae2772
Dimitri Couwenberg <couwenberg@gmail.com>
2017-12-03 15:08:49 +1100

    filter working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { sonnets } from './sonnets';
+import {Component} from '@angular/core';
+import {sonnets} from './sonnets';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,21 @@ import { sonnets } from './sonnets';
 export class AppComponent {
   title = "Shakespeare's Sonnets";
   sonnets = sonnets;
+  filteredData = sonnets;
+
+  search(val: any) {
+    let ind = false;
+    console.log(val);
+    if (!val) this.filteredData = this.sonnets;
+
+    this.filteredData = this.sonnets.filter((item) => {
+      return item.lines.filter((inner) => {
+        return inner.indexOf(val) >=0;
+      }).length > 0;
+    });
+
+    console.log('filteredData', this.filteredData);
+  }
 }
+
+
