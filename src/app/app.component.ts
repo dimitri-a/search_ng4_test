7ce4e8829a3a6e74b9455d284944511b10df9a0c
Dimitri Couwenberg <couwenberg@gmail.com>
2017-12-03 16:27:57 +1100

    udpate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,120 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
 import {sonnets} from './sonnets';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <input #search class="search" type="text" placeholder="Search..." (keydown.enter)="onSearch(search.value)">
+    <ng2-smart-table [settings]="settings" [source]="source"></ng2-smart-table>
+  `,
 })
 export class AppComponent {
-  title = "Shakespeare's Sonnets";
-  sonnets = sonnets;
-  filteredData = sonnets;
 
-  search(val: any) {
-    let ind = false;
-    console.log(val);
-    if (!val) this.filteredData = this.sonnets;
+  settings = {
+    columns: {
+      number: {
+        title: 'number',
+        filter: false,
+      },
+      lines: {
+        title: 'lines',
+        filter: false,
+      }
+    },
+  };
 
-    this.filteredData = this.sonnets.filter((item) => {
-      return item.lines.filter((inner) => {
-        return inner.indexOf(val) >=0;
-      }).length > 0;
-    });
+  // data = [
+  //   {
+  //     id: 1,
+  //     name: 'Leanne Graham',
+  //     username: 'Bret',
+  //     email: 'Sincere@april.biz',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Ervin Howell',
+  //     username: 'Antonette',
+  //     email: 'Shanna@melissa.tv',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Clementine Bauch',
+  //     username: 'Samantha',
+  //     email: 'Nathan@yesenia.net',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Patricia Lebsack',
+  //     username: 'Karianne',
+  //     email: 'Julianne.OConner@kory.org',
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Chelsey Dietrich',
+  //     username: 'Kamren',
+  //     email: 'Lucio_Hettinger@annie.ca',
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Mrs. Dennis Schulist',
+  //     username: 'Leopoldo_Corkery',
+  //     email: 'Karley_Dach@jasper.info',
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Kurtis Weissnat',
+  //     username: 'Elwyn.Skiles',
+  //     email: 'Telly.Hoeger@billy.biz',
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'Nicholas Runolfsdottir V',
+  //     username: 'Maxime_Nienow',
+  //     email: 'Sherwood@rosamond.me',
+  //   },
+  //   {
+  //     id: 9,
+  //     name: 'Glenna Reichert',
+  //     username: 'Delphine',
+  //     email: 'Chaim_McDermott@dana.io',
+  //   },
+  //   {
+  //     id: 10,
+  //     name: 'Clementina DuBuque',
+  //     username: 'Moriah.Stanton',
+  //     email: 'Rey.Padberg@karina.biz',
+  //   },
+  //   {
+  //     id: 11,
+  //     name: 'Nicholas DuBuque',
+  //     username: 'Nicholas.Stanton',
+  //     email: 'Rey.Padberg@rosamond.biz',
+  //   },
+  // ];
 
-    console.log('filteredData', this.filteredData);
+  data =sonnets;
+
+  source: LocalDataSource;
+
+  constructor() {
+    this.source = new LocalDataSource(this.data);
   }
+
+  // onSearch(query: string = '') {
+  //   this.source.setFilter([
+  //     // fields we want to inclue in the search
+  //     {
+  //       field: 'number',
+  //       search: query,
+  //     },
+  //     {
+  //       field: 'lines',
+  //       search: query,
+  //     }
+  //   ], false);
+  //   // second parameter specifying whether to perform 'AND' or 'OR' search
+  //   // (meaning all columns should contain search query or at least one)
+  //   // 'AND' by default, so changing to 'OR' by setting false here
+  // }
 }
-
-
